9cd7ba5197703f268dda2afd77020718038dbccd
nikol_vyskvarko <nikavuskvarko@gmail.com>
2022-01-09 20:34:28 +0300

    Creating methods CreateCountry CreateGenre CreateMovie CreateStudio

--- Model/DataWorker.cs
+++ Model/DataWorker.cs
@@ -1,22 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ManageMoviesDBApp.Model.Data;
+
 
 namespace ManageMoviesDBApp.Model
 {
     public static class DataWorker
     {
-        //create country
+        public static string CreateCountry(string name)
+        {
+            string result = "Already exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                bool isExsists = db.Countries.Any(country => country.Name == name);
+                if (!isExsists)
+                {
+                    Country newCountry = new Country { Name = name };
+                    db.Add(newCountry);
+                    db.SaveChanges();
+                    result = "It's done!";
+                }
+            }
+            return result;
+        }
 
-        //create genre
+        public static string CreateGenre(string name)
+        {
+            string result = "Already exsists!";
+            using(ApplicationContext db = new ApplicationContext())
+            {
+                bool isExsists = db.Genres.Any(genre => genre.Name == name);
+                if (!isExsists)
+                {
+                    Genre newGenre = new Genre { Name = name };
+                    db.Add(newGenre);
+                    db.SaveChanges();
+                    result = "It's done!";
+                }
+            }
+            return result;
+        }
 
-        //create movie
+        public static string CreateMovie(string name, List<Genre> genres, Studio studio, int year, double rating)
+        {
+            string result = "Already exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                bool isExsists = db.Movies.Any(movie => movie.Name == name);
+                if (!isExsists)
+                {
+                    Movie newMovie = new Movie
+                    {
+                        Name = name,
+                        Genres = genres,
+                        StudioId = studio.Id,
+                        Year = year,
+                        Rating = rating
+                    };
 
-        //create studio
+                    db.Add(newMovie);
+                    db.SaveChanges();
+                    result = "It's done!";
+                }
+            }
+            return result;
+        }
+
+        public static string CreateStudio(string name, Country country)
+        {
+            string result = "Already exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                bool isExsists = db.Studios.Any(studio => studio.Name == name);
+                if (!isExsists)
+                {
+                    Studio newStudio = new Studio 
+                    {
+                        Name = name,
+                        CountryId = country.Id
+                    };
+
+                    db.Add(newStudio);
+                    db.SaveChanges();
+                    result = "It's done!";
+                }
+            }
+            return result;
+        }
 
         //delete country
+        //public static string DeleteMovie(Movie movie)
+        //{
+
+        //}
+
 
         //delete genre
 
@@ -31,6 +108,6 @@ namespace ManageMoviesDBApp.Model
         //edit movie
 
         //edit studio
-        
+
     }
 }
