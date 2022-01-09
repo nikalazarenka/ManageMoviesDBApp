8735abd2b67a06fb16b2bf9d1a99bfcb60471a30
nikol_vyskvarko <nikavuskvarko@gmail.com>
2022-01-09 16:52:40 +0300

    Refactoring Creating dirs: Model, View, ViewModel Creating files: Country.cs, DataWorker.cs, Genre.cs, Movie.cs, Studio.cs Moving files MainWindow.xaml, MainWindow.xaml.cs into View dir.

---
+++ Model/DataWorker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageMoviesDBApp.Model
+{
+    public static class DataWorker
+    {
+        //create country
+
+        //create genre
+
+        //create movie
+
+        //create studio
+
+        //delete country
+
+        //delete genre
+
+        //delete movie
+
+        //delete studio
+
+        //edit country
+
+        //edit genre
+
+        //edit movie
+
+        //edit studio
+        
+    }
+}
