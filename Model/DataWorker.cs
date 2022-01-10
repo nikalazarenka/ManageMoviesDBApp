71955709b370e8c606fd2038eff5aec9fa7049ff
nikol_vyskvarko <nikavuskvarko@gmail.com>
2022-01-10 20:33:07 +0300

    Adding new functionality Completing class DataWorker

--- Model/DataWorker.cs
+++ Model/DataWorker.cs
@@ -7,6 +7,37 @@ namespace ManageMoviesDBApp.Model
 {
     public static class DataWorker
     {
+        public static List<Country> GetAllCountries()
+        {
+            using(ApplicationContext db = new ApplicationContext())
+            {
+                return db.Countries.ToList();
+            }
+        }
+
+        public static List<Genre> GetAllGenres()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                return db.Genres.ToList();
+            }
+        }
+
+        public static List<Movie> GetAllMovies()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                return db.Movies.ToList();
+            }
+        }
+
+        public static List<Studio> GetAllStudios()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                return db.Studios.ToList();
+            }
+        }
         public static string CreateCountry(string name)
         {
             string result = "Already exsists!";
@@ -85,29 +116,124 @@ namespace ManageMoviesDBApp.Model
                     result = "It's done!";
                 }
             }
+
             return result;
         }
 
-        //delete country
-        //public static string DeleteMovie(Movie movie)
-        //{
+        public static string DeleteCountry(Country country)
+        {
+            string result = "Not exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Countries.Remove(country);
+                db.SaveChanges();
+                result = "It's done! " + country.Name + " was deleted.";
+            }
 
-        //}
+            return result;
+        }
 
 
-        //delete genre
+        public static string DeleteGenre(Genre genre)
+        {
+            string result = "Not exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Genres.Remove(genre);
+                db.SaveChanges();
+                result = "It's done! " + genre.Name + " was deleted.";
+            }
 
-        //delete movie
+            return result;
+        }
 
-        //delete studio
+        public static string DeleteMovie(Movie movie)
+        {
+            string result = "Not exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Movies.Remove(movie);
+                db.SaveChanges();
+                result = "It's done! " + movie.Name + " was deleted.";
+            }
 
-        //edit country
+            return result;
+        }
 
-        //edit genre
+        public static string DeleteStudio(Studio studio)
+        {
+            string result = "Not exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Studios.Remove(studio);
+                db.SaveChanges();
+                result = "It's done! " + studio.Name + " was deleted.";
+            }
 
-        //edit movie
+            return result;
+        }
 
-        //edit studio
+        public static string EditCountry(Country oldCountry, string newName)
+        {
+            string result = "Not exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Country country = db.Countries.FirstOrDefault(c => c.Id == oldCountry.Id);
+                country.Name = newName;
+                db.SaveChanges();
+                result = "It's done!";
+            }
+
+            return result;
+        }
+
+        public static string EditGenre(Genre oldGenre, string newName)
+        {
+            string result = "Not exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Genre genre = db.Genres.FirstOrDefault(g => g.Id == oldGenre.Id);
+                genre.Name = newName;
+                db.SaveChanges();
+                result = "It's done!";
+            }
+
+            return result;
+        }
+
+        public static string EditMovie(Movie oldMovie, string newName, List<Genre> newGenres, Studio newStudio, int newYear, double newRating)
+        {
+            string result = "Not exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Movie movie = db.Movies.FirstOrDefault(m => m.Id == oldMovie.Id);
+                movie.Name = newName;
+                movie.Genres = newGenres;
+                movie.StudioId = newStudio.Id;
+                movie.Year = newYear;
+                movie.Rating = newRating;
+                db.SaveChanges();
+                result = "It's done!";
+            }
+
+            return result;
+        }
+
+        public static string EditStudio(Studio oldStudio, string newName, Country newCountry)
+        {
+            string result = "Not exsists!";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Studio studio = db.Studios.FirstOrDefault(s => s.Id == oldStudio.Id);
+                studio.Name = newName;
+                studio.CountryId = newCountry.Id;
+                db.SaveChanges();
+                result = "It's done!";
+            }
+
+            return result;
+        }
+
 
     }
 }
